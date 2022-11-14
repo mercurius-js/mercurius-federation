15b4a9e0b43a0c2d9fe25edb1faeb2ae5d384cc5
Davide Fiorello <davide@codeflyer.com>
2022-11-14 12:53:42 +0100

    feat: add graphql-tag support (#5)
    * feat: add graphql-tag support
    
    * feat: add support to list of DocumentNode as input

---
+++ test/federationWithMultipleGql.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const { test } = require('tap')
+const Fastify = require('fastify')
+const mercurius = require('mercurius')
+const gql = require('graphql-tag')
+const buildFederationSchema = require('../lib/federation')
+
+test('federation support using schema from buildFederationSchema', async t => {
+  const app = Fastify()
+  const schema1 = `
+    extend type Query {
+      me: User
+    }
+  `
+
+  const schema2 = `
+    extend type Mutation {
+      add(a: Int, b: Int): Int
+    }
+
+    type User @key(fields: "id") {
+      id: ID!
+      name: String
+      username: String
+    }
+  `
+
+  const resolvers = {
+    Query: {
+      me: () => ({
+        id: '1',
+        name: 'John',
+        username: '@john'
+      })
+    },
+    Mutation: {
+      add: (_, { a, b }) => a + b
+    },
+    User: {
+      __resolveReference: object => {
+        return {
+          id: object.id,
+          name: 'John',
+          username: '@john'
+        }
+      }
+    }
+  }
+
+  const schema = [gql(schema1), gql(schema2)]
+
+  const federationSchema = buildFederationSchema(schema)
+
+  app.register(mercurius, {
+    schema: federationSchema,
+    resolvers
+  })
+
+  await app.ready()
+
+  let query = '{ _service { sdl } }'
+  let res = await app.inject({ method: 'GET', url: `/graphql?query=${query}` })
+
+  t.same(JSON.parse(res.body), { data: { _service: { sdl: `${schema1}${schema2}` } } })
+
+  query = '{ me { id name username } }'
+  res = await app.inject({ method: 'GET', url: `/graphql?query=${query}` })
+  t.same(JSON.parse(res.body), {
+    data: { me: { id: '1', name: 'John', username: '@john' } }
+  })
+
+  query = 'mutation { add(a: 11 b: 19) }'
+  res = await app.inject({ method: 'POST', url: '/graphql', body: { query } })
+  t.same(JSON.parse(res.body), { data: { add: 30 } })
+})
--- test/types/index.ts
+++ test/types/index.ts
@@ -1,5 +1,6 @@
 import { expectType, expectError } from 'tsd'
 import Fastify from 'fastify'
+import gql from 'graphql-tag'
 import { GraphQLSchema } from 'graphql/index'
 
 import { buildFederationSchema, mercuriusFederationPlugin } from '../../index'
@@ -16,7 +17,15 @@ const schema = `
     }
   `
 
+const schema2 = `
+    extend type Query {
+      you: User
+    }
+  `
+
 expectType<GraphQLSchema>(buildFederationSchema(schema))
+expectType<GraphQLSchema>(buildFederationSchema(gql(schema)))
+expectType<GraphQLSchema>(buildFederationSchema([gql(schema), gql(schema2)]))
 expectType<GraphQLSchema>(buildFederationSchema(schema, {}))
 expectType<GraphQLSchema>(buildFederationSchema(schema, { isGateway: true }))
 
