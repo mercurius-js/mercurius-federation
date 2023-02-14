2c4695bc65725e8da2c543e179b29278e7888711
Marco Ippolito <marcoippolito54@gmail.com>
2023-02-14 19:03:13 +0100

    fix: updated default import (#13)
    * fix: updated default import
    
    * fix: keep both exports and add default

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const buildFederationSchema = require('./lib/federation')
 const mercuriusFederationPlugin = require('./lib/plugin')
 
 module.exports = { mercuriusFederationPlugin, buildFederationSchema }
+module.exports.default = mercuriusFederationPlugin
